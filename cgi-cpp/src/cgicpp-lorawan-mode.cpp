bda88e0d37fe6ec5fa37a6cae51cc5216c35254c
刘胜 <sheng.liu@minew.com>
2024-07-11 11:36:42 +0800

    perf(lora): 优化LoRaWAN region的获取

--- cgi-cpp/src/cgicpp-lorawan-mode.cpp
+++ cgi-cpp/src/cgicpp-lorawan-mode.cpp
@@ -124,39 +124,6 @@ int generate_gateway_id_by_mac(char *gw_id)
     return 0;
 }
 
-static void remove_comments(char *string, const char *start_token, const char *end_token)
-{
-    int    in_string = 0, escaped = 0;
-    size_t i;
-    char  *ptr             = NULL, current_char;
-    size_t start_token_len = strlen(start_token);
-    size_t end_token_len   = strlen(end_token);
-    if (start_token_len == 0 || end_token_len == 0) {
-        return;
-    }
-    while ((current_char = *string) != '\0') {
-        if (current_char == '\\' && !escaped) {
-            escaped = 1;
-            string++;
-            continue;
-        } else if (current_char == '\"' && !escaped) {
-            in_string = !in_string;
-        } else if (!in_string && strncmp(string, start_token, start_token_len) == 0) {
-            for (i = 0; i < start_token_len; i++) { string[i] = ' '; }
-            string = string + start_token_len;
-            ptr    = strstr(string, end_token);
-            if (!ptr) {
-                return;
-            }
-
-            for (i = 0; i < (ptr - string) + end_token_len; i++) { string[i] = ' '; }
-            string = ptr + end_token_len - 1;
-        }
-        escaped = 0;
-        string++;
-    }
-}
-
 static void get_hardware_region_info(string &region)
 {
     json     loacl_json;
